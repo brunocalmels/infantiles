dcc41acb8fbab6c6418ab31bf8c911eac9d43381
Bruno Calmels <brunocalmels@gmail.com>
2016-11-11 10:38:11 -0300

    Inclusion de formulario para datos de contacto en Organizacion, con JS

--- form-organizar.php
+++ form-organizar.php
@@ -8,8 +8,11 @@
 	$cuerpo = "<p>Hola. Alguien quiere ayuda con la organización de su boda en los siguientes rubros:</p>";
 	$cuerpo .= '<p>' . $rubros . '</p>';
 	$asunto = "Contacto desde el sitio web";
-	$headers = array('Content-Type: text/html; charset=UTF-8', 'From: Sitio web de Revista Bodas <revistabodas@revistabodas.com.ar>');
-	echo 'Mandando mail con cuerpo: ' . $cuerpo;
-	//wp_mail( $destinatarios, $asunto, $cuerpo, $headers );
-
+	$headers = array('Content-Type: text/html; charset=UTF-8', 'From: Sitio web de Revista Bodas <web@revistabodas.com.ar>');
+	$destinatarios = 'revistabodas@revistabodas.com.ar';
+	write_log( 'Mandando mail con cuerpo: ' . $cuerpo );
+	$res = wp_mail( $destinatarios, $asunto, $cuerpo, $headers );
+	if( $res )
+		echo "true";
+	echo "false";
 ?>
--- wp-content/themes/Divi-child/js/ajax_form.js
+++ wp-content/themes/Divi-child/js/ajax_form.js
@@ -2,7 +2,10 @@ function enviarMail(str) {
 	var ajax = new XMLHttpRequest();
   ajax.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-          alert(this.responseText);
+      	if (this.responseText.indexOf("true") > 0)
+          alert("Te contactaremos en breve para ayudarte a organizar el mejor día de tu vida.");
+        else
+        	alert("No se pudo enviar el formulario. Por favor, comunicate con nosotros telefónicamente y te atenderemos con gusto.");
       }
   };
   ajax.open("GET", varsGlobalesJS.homeUrl + "/form-organizar.php?rubros=" + str, true);
@@ -13,7 +16,20 @@ function enviarMail(str) {
 	$(document).ready(function(){
 
 		$('#org-submit').on({
-			click: function(){
+			click: function(e){
+				e.preventDefault();
+				// Validacion de campos de contacto completos
+				var email  = $('#org-email')[0].value;
+				var tel    = $('#org-tel')[0].value;
+				var nombre = $('#org-nombre')[0].value;
+				if (nombre == '' || nombre == null) {
+					alert('Por favor incluí un nombre.');
+					return;
+				}
+				if ((email == null || email == '') && (tel == "" || tel == null)) {
+					alert('Por favor incluí un email o un teléfono.');
+					return;
+				}
 				var lista = '';
 				var rubros = $('input.org-check');
 				for (i = 0; i < rubros.length; i++) {
